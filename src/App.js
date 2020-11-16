71521e2e9572571725e538e361e9903bd6638796
zhangqian5997 <873440829@qq.com>
2020-11-16 16:23:07 +0800

    f

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Login extends React.Component{
+    render(){
+      return(
+        <div className='content'>
+          <div className='login-box'>
+            <p>SmartToilet后台管理系统登录</p>
+            <div className='inputcontent'>
+              <from>
+                <input className='inputinner' placeholder='请输入账号' type='text'></input>
+                <input className='inputinner' placeholder='请输入密码' type='password'></input>
+                <button className='inputinner' >LOGIN</button>
+              </from>
+          </div>
+          </div>
+        </div>
+      );
+    }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Login from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Login />,
   document.getElementById('root')
 );
 
